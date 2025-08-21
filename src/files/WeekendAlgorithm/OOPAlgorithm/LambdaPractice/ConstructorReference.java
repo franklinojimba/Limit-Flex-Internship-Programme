560a742ac121c36076b222406e23ba0e0454ccc3
franklinojimba <ojimbachibuike825@gmail.com>
2025-08-21 12:09:13 +0100

    use of constructor reference to create a list of objects

---
+++ src/files/WeekendAlgorithm/OOPAlgorithm/LambdaPractice/ConstructorReference.java
@@ -0,0 +1,31 @@
+package files.WeekendAlgorithm.OOPAlgorithm.LambdaPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Person{
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+public class ConstructorReference {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Megan", "Alexander", "Bob"));
+        List<Person> person = names.stream().map(Person::new).toList();
+        System.out.println(person);
+
+        names.sort(String::compareToIgnoreCase);
+        System.out.println("Sorted names: " + names);
+    }
+}
